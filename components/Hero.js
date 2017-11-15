c1130c493d4a22044c22e69cc3f52778cc3e8850
João Justo <jpjustonunes@gmail.com>
2017-11-15 13:23:09 +0000

    Better youtube integration

--- components/Hero.js
+++ components/Hero.js
@@ -4,18 +4,28 @@ import '../css/Hero';
 import Text from './Text';
 import Logo from './Logo';
 import Button from './Button';
+import Youtube from 'react-youtube';
 import PageTitle from './PageTitle';
 import HeroBackgroundMobile from '../images/hero-mobile.jpg';
 import HeroBackgroundDesktop from '../images/hero@2x.jpg';
 
+const YoutubeVideoId = '1F23QJ_U2MI';
+const youtubeOpts = {
+  playerVars: {
+    autoplay: 1,
+    loop: 1,
+    controls: 0,
+    playlist: YoutubeVideoId
+  }
+};
+
 const Hero = () => (
   <section className="Hero">
-    <iframe className="Hero-background"
-      volume="0"
-      frameborder="0"
-      allowfullscreen
-      src="https://www.youtube.com/embed/1F23QJ_U2MI?controls=0&showinfo=0&rel=0&autoplay=1&loop=1">
-    </iframe>
+    <Youtube
+      className="Hero-background"
+      videoId={YoutubeVideoId}
+      opts={youtubeOpts}
+    />
     <div className="Hero-darkOverlay" />
     <div className="Hero-lightOverlay" />
     <div className="Hero-content">
@@ -27,7 +37,9 @@ const Hero = () => (
 
       <div className="Hero-actions">
         <Button to="mailto:info@justcome.pt">Contact</Button>
-        <Button alternative to="tel:+351918460295">Call Us</Button>
+        <Button alternative to="tel:+351918460295">
+          Call Us
+        </Button>
       </div>
     </div>
   </section>
